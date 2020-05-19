6145d82708be279c22b7ccc01d89aed2890621ab
EricKIm <kimhi65@gmail.com>
2020-05-19 15:28:00 +0900

    200519-1528-Kim

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/dabory/svc-abango/routers"
 
 	"github.com/dabory/abango"
 )
 
 func init() {
+	fmt.Print("svc-abango should be run in $GOSRC/github.com/dabory/svc-abango/")
 	abango.RunServicePoint(routers.KafkaRouter, routers.GrpcConnect, routers.RestConnect) // Do not change !!
 }
 
